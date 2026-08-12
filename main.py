32f980b0e18a1ffabc4af2c60e38570a78cfca8c
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2026-08-12 21:15:16 +0530

    fix: loading th env first

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from dotenv import load_dotenv
 
+load_dotenv()
+
 from display_clock import clock
 from utils.log import configure_logging
 
-load_dotenv()
 configure_logging()
 
 if __name__ == "__main__":
