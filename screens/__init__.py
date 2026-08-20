437637a6c924e20e2b99dcc4f3dbb46c7703490c
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2026-08-20 21:59:50 +0530

    refactor: made digital_clock the default screen

--- screens/__init__.py
+++ screens/__init__.py
@@ -13,7 +13,7 @@ AVAILABLE_SCREENS: dict[str, Callable[[], Screen]] = {
 }
 
 # Default screen name
-DEFAULT_SCREEN = "datetime_weather_forecast"
+DEFAULT_SCREEN = "digital_clock"
 
 
 def get_screen(name: str) -> Screen:
